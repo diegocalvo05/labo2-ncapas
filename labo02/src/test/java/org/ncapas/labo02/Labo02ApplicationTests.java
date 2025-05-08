8f307a691278b71f148666880236d2a5676632c0
diegocalvo05 <00103322@uca.edu.sv>
2025-05-08 16:39:04 -0600

    init spring boot project

---
+++ labo02/src/test/java/org/ncapas/labo02/Labo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ncapas.labo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
